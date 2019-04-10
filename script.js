18bfcaaeae610ba3dc4d37b013a8aa6f132a47b1
yannis94 <33447485+yannis94@users.noreply.github.com>
2019-04-10 11:29:44 +0200

    gtrht

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-let img = document.getElementById('test');
-let loop = 0;
-
-let yeah = document.getElementById('ok');
-let score = 0;
-
-img.addEventListener("click", function() {
-	console.log(loop);
-
-	for (loop; loop < 10; loop++) {
-
-		console.log("loop + 1")
-
-
-		if (loop == 6) {
-			img.style.backgroundColor = "blue";
-			console.log('fond bleu');
-		}
-	}
-
-	score++;
-	yeah.innerHTML = "<p>" + score + "</p>";
-
-	
-});
-
-
-
-
+function yeah() {
+	alert("Vous devez posséder un compte DeadSec ou alors vous y connecter si c'est déja le cas");
+}
